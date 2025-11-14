673bf168be64838e2c605c6b3e0d7c4911c77cea
nikola.lukic <zlatnaspirala@gmail.com>
2025-11-14 11:59:04 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # web-protect
+
 Disable debugger manipulation in runtime. Good for multiplayer gameplays solution or some kind of security important pages.
+
+Disclaimer
+
+This project is designed to protect web content by detecting active debugging tools and redirecting the user. Use it at your own responsibility. The intention of this project is strictly protective — not to harm, disrupt, or interfere with any legitimate user activity. Use ethically and in compliance with applicable laws.
---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * This file created by:
+ * @author Nikola Lukic 
+ * @email zlatnaspirala@gmail.com mart 2024
+ * @description npm import/export
+ */
+
+
+var about = () => {
+  console.info("Hi web-protect is ready...")
+  console.info("--------------------------------------------")
+  console.info("List of features: ")
+  console.info(" - Loading obj files with uvs")
+  console.info(" - Scene camera use -z front")
+  console.info("--------------------------------------------")
+}
+
+export {
+  about
+}
